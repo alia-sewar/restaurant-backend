fab777a384de187ccf6240fa468178e0862d6570
alia.sewar <130346497+alia-sewar@users.noreply.github.com>
2024-03-31 05:53:55 +0300

    Add Migration Files

---
+++ database/migrations/2024_03_31_023803_create_sub_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->unsignedBigInteger('category_id')->default(0);
+            $table->unsignedBigInteger('level_id')->default(0);
+            $table->unsignedInteger('added_by')->default(0);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+};
